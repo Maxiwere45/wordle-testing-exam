400bbd133233aebda6902f7d35015adafea9d9e0
Maxiwere45 <amdjad9857@gmail.com>
2025-03-29 00:52:14 +0100

    feat: Implement mini game on main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import {WordleGame} from "./data/modules/WordleGame.ts";
+import Player from "./data/modules/Player.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -19,6 +21,34 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+
+// Main game
+let player = new Player("Amdjad");
+let wordleGame = new WordleGame({ loadWordle: async () => ['p', 'o', 'm', 'm', 'e'] }, 'normal', player);
+
+(async () => {
+    await wordleGame.loadWordle();
+
+    wordleGame.play(['p', 'o', 'm', 'm', 'e']);
+    console.log(player.getAttempts());
+})();
+
+let wordleGame2 = new WordleGame({ loadWordle: async () => ['t', 'u', 'i', 'l', 'e'] }, 'normal', player);
+(async () => {
+    await wordleGame2.loadWordle();
+
+    wordleGame2.play(['p', 'e', 'u', 'e', 'e']);
+    console.log(wordleGame2.getColorsChecking());
+    wordleGame2.play(['t', 'u', 'i', 'l', 'e']);
+    console.log(wordleGame2.getColorsChecking());
+    console.log(player.getAttempts());
+    console.log(player.getStreaks());
+    console.log(player.getScore());
+})();
+
+
+
+
